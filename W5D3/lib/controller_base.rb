f179e9a7e42dce76b37b88ac151c1e6d887f5d88
tiffanyw-hu <34973286+tiffanyw-hu@users.noreply.github.com>
2018-03-20 12:39:06 -0700

    Add files via upload

---
+++ W5D3/lib/controller_base.rb
@@ -0,0 +1,63 @@
+require 'active_support'
+require 'active_support/core_ext'
+require 'erb'
+require_relative './session'
+
+class ControllerBase
+  attr_reader :req, :res, :params
+
+  # Setup the controller
+  def initialize(req, res)
+    @req, @res = req, res
+    @already_built_response = false
+  end
+
+  # Helper method to alias @already_built_response
+  def already_built_response?
+    @already_built_response
+  end
+
+  # Set the response status code and header
+  def redirect_to(url)
+    raise "error" if already_built_response?
+    res.status = 302
+    res.headers['Location'] = url
+    @already_built_response = true
+  end
+
+  # Populate the response with content.
+  # Set the response's content type to the given type.
+  # Raise an error if the developer tries to double render.
+  def render_content(content, content_type)
+    if already_built_response?
+      raise "error"
+    else
+      res['Content-Type'] = content_type
+      res.write(content)
+      @already_built_response = true
+    end
+  end
+
+  # use ERB and binding to evaluate templates
+  # pass the rendered html to render_content
+  def render(template_name)
+    dir_path = File.dirname(__FILE__)
+    template_fname = File.join(
+      dir_path, "..",
+      "views", self.class.name.underscore, "#{template_name}.html.erb"
+    )
+    file = File.read(template_fname)
+
+    render_content( ERB.new(file).result(binding), 'text/html')
+
+  end
+
+  # method exposing a `Session` object
+  def session
+    @session = Session.new
+  end
+
+  # use this with the router to call action_name (:index, :show, :create...)
+  def invoke_action(name)
+  end
+end
